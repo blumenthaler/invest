299061d2dc7dc3860762fe930287781a2eadc126
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-05 20:31:49 +0000

    assigned urls to topics in @@all array

--- lib/invest/topic.rb
+++ lib/invest/topic.rb
@@ -12,14 +12,23 @@ class Invest::Topic
   # array of all urls in the same order as @topic_array
     @url_array = doc.css("a.dictionary-top24-list__sublist.mntl-text-link").map{|n| n.attribute("href").value}
     
-    # @topics_array.each do |topic|
-    #   new_topic = Invest::Topic.new
-    #   new_topic.name = topic
-    #   new_topic.url = * an href attribute that contains topic name * doc.css("a.dictionary-top24-list__sublist.mntl-text-link").attribute("href").value
-    #   new_topic.definition =
-    #   new_topic.takeaways = 
-    #   @@all << new_topic
-    # end
+     @topics_array.each do |topic|
+       new_topic = Invest::Topic.new
+       new_topic.name = topic
+       @@all << new_topic
+     end
+     
+     @@all.each_with_index do |topic, index|
+       topic.url = @url_array[index]
+     end
+      binding.pry
+  # assign urls to topics??
+      # opened_url = Nokogiri::HTML(open(new_topic.url))
+      # new_topic.definition = opened_url.css("p#mntl-sc-block_1-0-1").text.strip
+      # new_topic.takeaways = opened_url.css("div#mntl-sc-block-callout-body_1-0 ul li").text.strip
+      # binding.pry
+      # @@all << new_topic
+  #   end
     
   # These topics need to not be hard coded...
   # Scrape the webpage, instantiate a Topic, take data from scrape & apply to attrs
@@ -39,19 +48,19 @@ class Invest::Topic
     
     
     
-    name = doc.css("a#dictionary-top24-list__sublist_1-0-38 span.link__wrapper").text.strip
-    url = doc.css("a#dictionary-top24-list__sublist_1-0-38").attribute('href').value
-    doc_2 = Nokogiri::HTML(open(url))
-    takeaways = doc_2.css("div#mntl-sc-block-callout-body_1-0 ul li").text.strip
-    definition = doc_2.css("p#mntl-sc-block_1-0-1").text.strip
-    topic = Invest::Topic.new
-    topic.name = name
-    topic.definition = definition
-    topic.takeaways = takeaways
-    topic.url = url
-    @topics << topic
-    binding.pry
-    @topics
+    # name = doc.css("a#dictionary-top24-list__sublist_1-0-38 span.link__wrapper").text.strip
+    # url = doc.css("a#dictionary-top24-list__sublist_1-0-38").attribute('href').value
+    # doc_2 = Nokogiri::HTML(open(url))
+    # takeaways = doc_2.css("div#mntl-sc-block-callout-body_1-0 ul li").text.strip
+    # definition = doc_2.css("p#mntl-sc-block_1-0-1").text.strip
+    # topic = Invest::Topic.new
+    # topic.name = name
+    # topic.definition = definition
+    # topic.takeaways = takeaways
+    # topic.url = url
+    # @topics << topic
+    # binding.pry
+    # @topics
   end
   
   def self.all
