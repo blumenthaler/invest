ed850520dd09e55283d259dc9f01684d052be7dc
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-07 19:24:56 +0000

    updated scraper class

--- lib/invest/scraper.rb
+++ lib/invest/scraper.rb
@@ -4,36 +4,42 @@ class Invest::Scraper
     Nokogiri::HTML(open("https://www.investopedia.com/financial-term-dictionary-4769738"))
   end
   
+# I do not want to scrape hundreds of topics at once
+# so Scraper.scrape_topic_page_from_input will scrape based on input (scrape by first character)
+# that method will return an array of urls of topics with the same first letter
+# Scraper.make_topics will use that array to create topic class instances
+  
   def self.scrape_topic_page_from_input(input)
     alphabet_front = ("A".."M").to_a
     alphabet_back = ("N".."Z").to_a
     
     if input == "#"
-      self.get_page.css("div#dictionary-top24-list__sublist-content_1-0 a").map{|n| n.attribute("href").value}
+      @urls = self.get_page.css("div#dictionary-top24-list__sublist-content_1-0 a").map{|n| n.attribute("href").value}
     elsif input != "exit"
       if alphabet_front.include?(input)
         alphabet_front.each_with_index do |letter, index|
           if input.chr == letter
-            self.get_page.css("div#dictionary-top24-list__sublist-content_1-0-#{index + 1} a").map{|n| n.attribute("href").value}
+            @urls = self.get_page.css("div#dictionary-top24-list__sublist-content_1-0-#{index + 1} a").map{|n| n.attribute("href").value}
           end
         end
       elsif alphabet_back.include?(input)
         alphabet_back.each_with_index do |letter, index|
           if input.chr == letter && letter == "N"
-            self.get_page.css("div#dictionary-top24-list__sublist-content_2-0 a").map{|n| n.attribute("href").value}
+            @urls = self.get_page.css("div#dictionary-top24-list__sublist-content_2-0 a").map{|n| n.attribute("href").value}
           elsif input.chr == letter && letter != "N"
-           self.get_page.css("div#dictionary-top24-list__sublist-content_2-0-#{index} a").map{|n| n.attribute("href").value}
+            @urls = self.get_page.css("div#dictionary-top24-list__sublist-content_2-0-#{index} a").map{|n| n.attribute("href").value}
           end
         end  
       end
     end
   end
-        # makes restaurant objects from index
-  # def make_topics
-  #   scrape_topic_page_from_input(input).each do |r|
-  #     Invest::Topic.new_from_index_page(r)
-  #   end
-  # end
+
+# makes topic object instances from index
+  def make_topics
+    @urls.each do |r|
+      Invest::Topic.new_from_index_page(r)
+    end
+  end
 
 #   def scrape_dictionary_for_terms(input)
   
